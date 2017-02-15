ba0b262887259b8a29e55fd77df4537ddcb536cd
JAVIER\Javier <javierpastorserrano@gmail.com>
2017-02-15 19:35:29 +0000

    AudioMixer

---
+++ Music as Gameplay/Assets/C#/AudioPlayNoMuteMusic.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioPlayNoMuteMusic : MonoBehaviour {
+
+  public float SecondsToStartMusic;
+  AudioSource AudioSource;
+  bool Play;
+  // Use this for initialization
+  void Start () {
+    AudioSource = GetComponent<AudioSource>();
+    Play = false;
+  }
+	
+	// Update is called once per frame
+	void Update () {
+
+    if (SecondsToStartMusic <= 0.0f)
+    {
+      if (!Play)
+      {
+        AudioSource.Play();
+        Play = true;
+      }
+    }
+    else
+    {
+      SecondsToStartMusic -= Time.deltaTime;
+    }
+  }
+}
