7f372aa6d04468912b13bf747c12da5a2966522b
JAVIER\Javier <javierpastorserrano@gmail.com>
2017-03-09 18:22:27 +0000

    Pause Menu and Main Menu changes

---
+++ Music as Gameplay/Assets/C#/MainMenu/MainMenuManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour {
+
+  public Transform InitialMenu;
+  public Transform CreditsMenu;
+
+	// Use this for initialization
+	void Start ()
+  {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+	
+	}
+
+  public void Play(int scene)
+  {
+    SceneManager.LoadScene(scene);
+  }
+  
+  public void Exit()
+  {
+    Application.Quit();
+  }
+
+  public void BackFromCreditsMenu()
+  {
+    if (CreditsMenu.gameObject.activeInHierarchy)
+    {
+      InitialMenu.gameObject.SetActive(true);
+      CreditsMenu.gameObject.SetActive(false);
+    }
+  }
+  
+  public void AviteCreditsMenu()
+  {
+    if (InitialMenu.gameObject.activeInHierarchy)
+    {
+      InitialMenu.gameObject.SetActive(false);
+      CreditsMenu.gameObject.SetActive(true);
+    }
+  }
+
+}
---
+++ Music as Gameplay/Assets/C#/PauseMenu/PauseMenuManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PauseMenuManager : MonoBehaviour {
+
+  public Transform PauseMenu;
+  public AudioSource MuteMusic;
+  public AudioSource NoMuteMusic;
+
+  // Use this for initialization
+  void Start ()
+  {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+    ActivatePauseMenu();
+	}
+
+  void ActivatePauseMenu()
+  {
+    if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+    {
+      if (!PauseMenu.gameObject.activeInHierarchy)
+      {
+        PauseMenu.gameObject.SetActive(true);
+        MuteMusic.Pause();
+        NoMuteMusic.Pause();
+        Time.timeScale = 0;
+      }
+    }
+  }
+
+  public void DesactivatePauseMenu()
+  {
+    if (PauseMenu.gameObject.activeInHierarchy)
+    {
+      PauseMenu.gameObject.SetActive(false);
+      MuteMusic.UnPause();
+      NoMuteMusic.UnPause();
+      Time.timeScale = 1;
+    }
+  }
+
+  public void GoMainMenu(int scene)
+  {
+    SceneManager.LoadScene(scene);
+  }
+}
